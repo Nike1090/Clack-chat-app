f209f4b39abeb3361c61a7286b749c6b91b9e350
Nikhil <bavandla.n@northeastern.edu>
2023-10-25 12:41:59 -0400

    login page

---
+++ js/ImageToggle.js
@@ -0,0 +1,26 @@
+
+
+function toggleImage() {
+    const iconImage = document.getElementById('iconImage');
+    const bodyElement = document.querySelector('body');
+    const formCard = document.querySelector('.card');
+
+
+    if (iconImage.src.includes('sunny.png')) {
+        iconImage.src = iconImage.src.replace('sunny.png', 'moon.png'); // Replace the image source
+        bodyElement.style.backgroundColor = "#fff";
+        bodyElement.style.color = "#181924";
+        formCard.style.color = "#181924";
+        formCard.style.backgroundColor = "#fff";
+        formCard.style.border = "rgb(33,32,50) solid 1px";
+        
+    } else {
+        iconImage.src = iconImage.src.replace('moon.png', 'sunny.png'); // Replace the image source
+        bodyElement.style.backgroundColor = "";
+        bodyElement.style.color = "";
+        formCard.style.color = "";
+        formCard.style.backgroundColor = "";
+        formCard.style.border = "";
+    }
+    
+}
